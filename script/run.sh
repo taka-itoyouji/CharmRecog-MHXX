3ab92676be8f6df1d03ade4000fc9f8c97ffe50f
taka-itoyouji <taka.bass0114@gmail.com>
2022-04-30 15:22:16 +0000

    updated.

--- script/run.sh
+++ script/run.sh
@@ -7,15 +7,5 @@ pngdir=../data/input
 outdir=../out/$dataset
 tooldir=../tool
 
-# スキルリストからスキルポイントを取得
-# cat ~/database/monsterhunter/XX/skill_list.csv | grep ,+ | cut -d ',' -f 1 > ../out/skill_list.txt
-
 #main
 python $tooldir/main.py --outdir $outdir --pngdir $pngdir
-
-# dataset=test
-
-# outdir=../out/$data
-
-# # test
-# python $tooldir/main.py --outdir $outdir
